2da0d95e208bdf9b85fbd010b25a233828e91bf6
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2024-03-07 23:55:27 +0600

    bug name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from '@/components/Hero';
-import Pricing from '@/components/Pricings';
+import Pricing from '@/components/Pricing';
 import SiteStats from '@/components/SiteStats';
 import TrustedBy from '@/components/TrustedBy';
 
