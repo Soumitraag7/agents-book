c6f9ed1a920c8222aa42fe8919781d9fed4f0bd4
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2024-03-13 14:49:33 +0600

    auth added redirect link and navbar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 
 interface AuthLayoutProps {
@@ -6,8 +7,19 @@ interface AuthLayoutProps {
 
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
 	return (
-		// <div className="dark:bg-[#19191a] dark:border-none border sm:w-[400px] mx-auto p-10 rounded-2xl">
-		<div className="flex items-center justify-center mx-auto">{children}</div>
+		<main>
+			<nav className="w-full relative flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 py-5">
+				<Link href="/" className="">
+					<h1 className="text-2xl">
+						agents<span className="text-[#d90706] font-bold">Book</span>
+					</h1>
+				</Link>
+			</nav>
+			{/* <div className="dark:bg-[#19191a] dark:border-none border sm:w-[400px] mx-auto p-10 rounded-2xl"> */}
+			<div className="flex items-center justify-center mx-auto p-7">
+				{children}
+			</div>
+		</main>
 	);
 };
 
