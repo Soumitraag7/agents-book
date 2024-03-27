c2a0ea67bbd4f9113d15ca1e507b18f32b7dd829
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2024-03-27 20:58:21 +0600

    fix: pricing.tsx rename

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import Hero from '@/app/(root)/_components/hero';
-import Pricing from '@/components/pricing';
+import Pricing from '@/app/(root)/_components/pricing';
 import SiteStatus from '@/app/(root)/_components/site-status';
 import TrustedBy from '@/app/(root)/_components/trusted-by';
 
