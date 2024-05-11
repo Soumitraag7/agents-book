bb2d44555ec338b804e85d9cc71636ff346a2323
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2024-05-11 22:34:01 +0600

    add: post card

--- src/app/dashboard/_components/post.tsx
+++ src/app/dashboard/_components/post.tsx
@@ -8,70 +8,42 @@ import Link from 'next/link';
 import { format } from 'date-fns';
 import { deletePost } from '@/lib/actions/post.action';
 
-export const DEMO_CARD_INFO = [
-	{
-		from: 'LAX',
-		to: 'YYX',
-		arivalTime: '20:30',
-		departureTime: '12:30',
-		arivalDate: '12-11-2022',
-		departureDate: '02-12-2023',
-		totalTicket: 0,
-		airlineName: 'Etihad Airways',
-		pricePerTicket: 0,
-		seatType: 'First'
-	},
-	{
-		from: 'LAX',
-		to: 'YYX',
-		arivalTime: '20:30',
-		departureTime: '12:30',
-		arivalDate: '12-11-2022',
-		departureDate: '02-12-2023',
-		totalTicket: 0,
-		airlineName: 'Etihad Airways',
-		pricePerTicket: 0,
-		seatType: 'First'
-	},
-	{
-		from: 'YYX',
-		to: 'LAX',
-		arivalTime: '20:30',
-		departureTime: '12:30',
-		arivalDate: '12-11-2022',
-		departureDate: '02-12-2023',
-		totalTicket: 0,
-		airlineName: 'Etihad Airways',
-		pricePerTicket: 0,
-		seatType: 'First'
-	}
-];
-
 type Props = {
 	posts: POSTS[];
 };
 
 export default function Post({ posts }: Props) {
 	return (
-		<div className=" grid  gap-5">
+		<div className=" grid gap-5">
 			{posts.map(post => (
 				<Card key={post._id}>
 					<CardContent className="mt-5 flex flex-col md:flex-row items-center justify-center gap-2">
-						<div className="flex flex-col md:flex-row items-center justify-center gap-3 text-sm">
-							<h2>From: {post.from}</h2>
-							<h2>To: {post.to}</h2>
-							<h2>DepartureTime: {post.departureTime}</h2>
-							<h2>Arival Time: {post.arivalTime}</h2>
-							<h2>Departure Date: {format(post.departureDate, 'LLL dd, y')}</h2>
-							<h2>Arival Date: {format(post.arivalDate, 'LLL dd, y')}</h2>
-							<h2>Airline Name: {post.airlineName}</h2>
-							<h2>Total Ticket: {post.totalTicket}</h2>
-							<h2>Price Per Ticket: {post.pricePerTicket}</h2>
-							<h2>Seat Type: {post.seatType}</h2>
+						<div className="flex flex-col md:flex-row items-center justify-between w-full gap-3 text-sm">
+							<h2>{post.airlineName}</h2>
+
+							<div className="flex flex-col items-end">
+								<h2>{post.departureTime}</h2>
+								<h2>{post.from}</h2>
+								<h2>{format(post.departureDate, 'LLL dd, y')}</h2>
+							</div>
+
+							<div className="flex flex-col items-end">
+								<h2>{post.arivalTime}</h2>
+								<h2>{post.to}</h2>
+								<h2>{format(post.arivalDate, 'LLL dd, y')}</h2>
+							</div>
+
+							<div className="flex flex-col items-end">
+								<h2>Tk {post.pricePerTicket} /Ticket</h2>
+								<h2>Total Ticket: {post.totalTicket}</h2>
+								<h2>
+									<span className="capitalize">{post.seatType}</span>
+								</h2>
+							</div>
 						</div>
 
-						<div className="flex items-center justify-center gap-3 ">
-							<Button asChild size="sm" className="w-full mt-7">
+						<div className="flex items-center justify-center gap-3 ml-3">
+							<Button asChild size="sm" className="w-full cursor-pointer">
 								<div className="gap-1.5">
 									<FilePenLine className="h-4 w-4 text-black" />
 									<span>Edit</span>
@@ -82,7 +54,7 @@ export default function Post({ posts }: Props) {
 								asChild
 								size="sm"
 								variant="destructive"
-								className="w-full mt-7"
+								className="w-full cursor-pointer"
 								onClick={() => {
 									deletePost(post._id);
 								}}
