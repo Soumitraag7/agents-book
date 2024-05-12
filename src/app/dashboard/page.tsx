37647989d5e3424600d6b769e1362358250c3af0
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2024-05-13 00:46:37 +0600

    add: post-card component to the dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,15 +3,21 @@
 import PageTitle from '@/app/dashboard/_components/page-title';
 import PostCard from '@/components/post-card';
 import SearchForm from '@/components/search-form';
+import { getAllPosts } from '@/lib/actions/post.action';
+import { POSTS } from '@/app/dashboard/my-posts/page';
 
-export default function Dashboard() {
+export const revalidate = 30; // revalidate at most 30 seconds
+
+export default async function Dashboard() {
+	const posts: POSTS[] = await getAllPosts();
+
 	return (
 		<main className="flex flex-col gap-5  w-full">
 			<PageTitle title="Dashboard" />
 
 			<SearchForm />
 
-			<PostCard />
+			<PostCard posts={posts} />
 		</main>
 	);
 }
--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -6,22 +6,35 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
-export default function PostCard() {
+import DemoImg from '../../public/images/demo-img.jpg';
+import { POSTS } from '@/app/dashboard/my-posts/page';
+
+type Props = {
+	posts: POSTS[];
+};
+
+export default function PostCard({ posts }: Props) {
+	console.log('D length:: ', posts.length);
+
 	return (
 		<div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-			{DEMO_CARD_INFO.map((post, idx) => (
-				<Card key={idx}>
-					<Image
+			{/* {DEMO_CARD_INFO.map((post, idx) => ( */}
+			{posts.map(post => (
+				<Card key={post._id}>
+					{/* <Image
 						// src={urlFor(post.titleImage).url()}
-						src={'/images/demo-img.jpg'}
+						// src={'/images/demo-img.jpg'}
+						src={DemoImg}
 						alt="image"
 						width={500}
 						height={500}
 						className="rounded-t-lg h-[200px] object-cover"
-					/>
+					/> */}
 
 					<CardContent className="mt-5">
-						<h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+						<h3 className="text-lg line-clamp-2 font-bold">
+							{post.airlineName}
+						</h3>
 
 						{/* <div className="flex items-center justify-between py-2 mt-2">
 							<Avatar className="w-7 h-7">
@@ -35,22 +48,28 @@ export default function PostCard() {
 						</div> */}
 
 						<div className="flex items-center justify-between">
-							<p>
-								Posted: <span className="text-gray-500">3hr ago</span>
+							<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
+								{post.from}
 							</p>
-							<p>
-								Price: <span className="text-gray-500">${post.price}</span>
+
+							<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
+								{post.to}
 							</p>
 						</div>
 
-						<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
-							{post.smallDescription}
-						</p>
+						<div className="flex items-center justify-between">
+							<p>
+								Ticket&apos;s:{' '}
+								<span className="text-gray-500">{post.totalTicket}</span>
+							</p>
+							<p>
+								Price:{' '}
+								<span className="text-gray-500">Tk {post.pricePerTicket}</span>
+							</p>
+						</div>
 
 						<Button asChild className="w-full mt-7">
-							<Link href={`/dashboard/post/${post.currentSlug}`}>
-								Read More
-							</Link>
+							<Link href={`/dashboard/post/${post._id}`}>Read More</Link>
 						</Button>
 					</CardContent>
 				</Card>
