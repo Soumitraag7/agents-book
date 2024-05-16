5acd401b4921d8285a259e2eaf0c259356519b2b
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2024-05-16 18:06:35 +0600

    refactor: post card component

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import { DEMO_CARD_INFO } from '@/constants';
+import { format } from 'date-fns';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
-import DemoImg from '../../public/images/demo-img.jpg';
 import { POSTS } from '@/app/dashboard/my-posts/page';
 
 type Props = {
@@ -14,27 +12,17 @@ type Props = {
 };
 
 export default function PostCard({ posts }: Props) {
-	console.log('D length:: ', posts.length);
-
 	return (
 		<div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-			{/* {DEMO_CARD_INFO.map((post, idx) => ( */}
 			{posts.map(post => (
 				<Card key={post._id}>
-					{/* <Image
-						// src={urlFor(post.titleImage).url()}
-						// src={'/images/demo-img.jpg'}
-						src={DemoImg}
-						alt="image"
-						width={500}
-						height={500}
-						className="rounded-t-lg h-[200px] object-cover"
-					/> */}
-
 					<CardContent className="mt-5">
-						<h3 className="text-lg line-clamp-2 font-bold">
-							{post.airlineName}
-						</h3>
+						<div className="flex flex-col items-center justify-between">
+							<h3 className="text-lg line-clamp-2 font-bold">
+								{post.airlineName}
+							</h3>
+							<p>{format(post.departureDate, 'LLL dd, y')}</p>
+						</div>
 
 						{/* <div className="flex items-center justify-between py-2 mt-2">
 							<Avatar className="w-7 h-7">
@@ -47,22 +35,22 @@ export default function PostCard({ posts }: Props) {
 							</Button>
 						</div> */}
 
-						<div className="flex items-center justify-between">
-							<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
+						<div className="flex items-center justify-between gap-4">
+							<p className="line-clamp-2 text-base mt-2 text-gray-600 dark:text-gray-300">
 								{post.from}
 							</p>
 
-							<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
+							<p className="line-clamp-2 text-base mt-2 text-gray-600 dark:text-gray-300">
 								{post.to}
 							</p>
 						</div>
 
 						<div className="flex items-center justify-between">
-							<p>
+							<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
 								Ticket&apos;s:{' '}
 								<span className="text-gray-500">{post.totalTicket}</span>
 							</p>
-							<p>
+							<p className="line-clamp-2 text-sm mt-2 text-gray-600 dark:text-gray-300">
 								Price:{' '}
 								<span className="text-gray-500">Tk {post.pricePerTicket}</span>
 							</p>
